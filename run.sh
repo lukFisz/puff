867bed995d69f8ae206d6ed7ff7ee6d79478a54f
Łukasz Fiszer <git@fisz.dev>
2025-11-22 22:27:03 +0100

    rm test webhook

--- run.sh
+++ run.sh
@@ -10,6 +10,6 @@ export PUFF_DELUGE_PASSWORD="deluge"
 export PUFF_DRY_RUN="true"
 export PUFF_RETENTION="P5D"
 export PUFF_LOG_LEVEL="debug"
-export PUFF_DISCORD_WEBHOOK_URL="https://discord.com/api/webhooks/1392149317812486358/hDQ2v2kzTqITcr_Qt5oQEsaFBu7woFq5lu2jDn_fFfTSfBVtKqDYtGDbHHY2SxITbyTM"
+export PUFF_DISCORD_WEBHOOK_URL=""
 
 ./target/puff
