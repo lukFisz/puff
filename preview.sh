677a96a2f582c0242389c9fa0d454a353641cdc4
Łukasz Fiszer <git@fisz.dev>
2025-12-11 23:14:27 +0100

    ci: minor improvments, changing preview flow

--- preview.sh
+++ preview.sh
@@ -2,6 +2,36 @@
 
 set -e
 
-docker pull ghcr.io/lukfisz/puff:latest
+export PVER=$1
 
+if [ -z "$PVER" ]; then
+    echo "Specify version:"
+    echo "> ./preview.sh VERSION"
+    exit 1
+fi
+
+echo "-----------------"
+echo "Builder"
+echo "-----------------"
+docker build \
+    -f Dockerfile-builder \
+    . -t puff-builder
+docker run -v "./target:/output" --rm puff-builder
+
+echo "-----------------"
+echo "Image (using version label: $PVER)"
+echo "-----------------"
+docker build \
+    --build-arg PUFF_CURRENT_VERSION=$PVER \
+    -f Dockerfile \
+    ./target -t ghcr.io/lukfisz/puff:latest
+
+echo "-----------------"
+echo "VHS"
+echo "-----------------"
 vhs preview.tape
+
+echo "-----------------"
+echo "Cleaning"
+echo "-----------------"
+rm -f ./target/puff_*
